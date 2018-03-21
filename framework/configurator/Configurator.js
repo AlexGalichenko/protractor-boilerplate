fcc18f260c1a1db7c0fc20fbda9d1fa518ea157d
Oleksandr Halichenko <Oleksandr.Halichenko@wolterskluwer.com>
2018-03-21 17:59:58 +0300

    added jsDocs for Configurator class

--- framework/configurator/Configurator.js
+++ framework/configurator/Configurator.js
@@ -4,12 +4,20 @@ const CredentialManager = require("../credential_manager/ServerCredentialManager
 
 class Configurator {
 
+    /**
+     * Constructor of configuration object
+     * @param params
+     */
     constructor(params) {
         this.pageMap = params.pageMap;
         this.calculablesMap = params.calculablesMap;
         this.constantMap = params.constantMap;
     }
 
+    /**
+     * Builds configuration and returns configured State, Memory etc. classes
+     * @return {{State: State, Memory: Memory, CredentialManager: ServerCredentialManager}}
+     */
     build() {
         State.setPageMap(this.pageMap);
         Memory.setCalculables(this.calculablesMap);
