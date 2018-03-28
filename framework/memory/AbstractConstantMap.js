9cd66f71eb83b6a2b71ce6956b0577f681ce735d
Oleksandr Halichenko <Oleksandr.Halichenko@wolterskluwer.com>
2018-03-28 17:17:46 +0300

    add possibility to define file constants

--- framework/memory/AbstractConstantMap.js
+++ framework/memory/AbstractConstantMap.js
@@ -1,12 +1,19 @@
-"use strict";
+const path = require("path");
+const fs = require("fs");
 
 class AbstractConstantMap {
 
     constructor() {
         this.constants = {};
+        this.fileConstants = {};
     }
 
-    setConstant(key, givenValue) {
+    /**
+     * Define constant
+     * @param key - key
+     * @param givenValue - value
+     */
+    defineConstant(key, givenValue) {
         const onlyReadableObject = {};
         if (this.constants.hasOwnProperty(key)) {
             throw new Error(`Constant with such key: '${key}' is already created`);
@@ -19,11 +26,38 @@ class AbstractConstantMap {
         }
     }
 
+    /**
+     * Define file constant
+     * @param key - key of constant
+     * @param path - path to file
+     */
+    defineFileConstant(key, path) {
+        this.fileConstants[key] = path;
+    }
+
+    /**
+     * Get constant value by key
+     * @param key - key of file constant
+     * @return {String}
+     */
     getConstant(key) {
         if (!this.constants.hasOwnProperty(key)) {
             throw new Error(`No such key: '${key}'`);
         }
-        return this.constants[key][key];
+        return this.constants[key];
+    }
+
+    /**
+     * Get file constant value by key
+     * @param key - key of file constant
+     * @return {String} - file content
+     */
+    getFileConstant(key) {
+        if (this.fileConstants[key]) {
+            return fs.readFileSync(path.resolve(this.fileConstants[key]))
+        } else {
+            throw new Error(`No such key: '${key}'`);
+        }
     }
 
 }
