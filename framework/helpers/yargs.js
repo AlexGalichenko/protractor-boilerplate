546659a78b35433efe4d80d20b9e6a1724d7e7b6
Patsyna, Dzianis <dzianis_patsyna@epam.com>
2018-05-11 15:13:14 +0300

    added yargs

---
+++ framework/helpers/yargs.js
@@ -0,0 +1,28 @@
+module.exports.argv = require("yargs")
+    .option("env", {
+        demandOption: true,
+        describe: "environment",
+        message: "please set --env argument"
+    })
+    .option("tags", {
+        describe: "tags",
+    })
+    .option("credentialServerPort", {
+        alias: "csp",
+        describe: "credentialServerPort",
+    })
+    .option("browserName", {
+        alias: "browser",
+        describe: "browserName",  
+    })
+    .option("maxInstances", {
+        alias: "i",
+        describe: "maxInstances",   
+    })
+    .option("baseUrl", {
+        describe: "baseUrl",
+    })
+    .option("debug", {
+        describe: "debug",
+    })
+    .argv;
