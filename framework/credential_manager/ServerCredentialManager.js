3d54010f29235aff86d8ffbc33fcc4dceef5bff8
Oleksandr Halichenko <Oleksandr.Halichenko@wolterskluwer.com>
2018-10-31 16:10:15 +0300

    updated credential manager to support custom pools

--- framework/credential_manager/ServerCredentialManager.js
+++ framework/credential_manager/ServerCredentialManager.js
@@ -12,13 +12,18 @@ class ServerCredentialManager {
     /**
      * Create pool of userIds based on creds object
      * @param {Object} creds - set of user credentials
+     * @param {string} pool - name of pool
      * @throws {Error}
      * @example CredentialManager.createPool(credentials);
      */
-    static createPool(creds) {
+    static createPool(creds, pool) {
+        let requestURI = SERVICE_URI;
+        if (pool) {
+            requestURI += "?pool=" + pool;
+        }
         return request({
             method: "POST",
-            uri: SERVICE_URI,
+            uri: requestURI,
             body: creds,
             json: true
         })
@@ -29,34 +34,44 @@ class ServerCredentialManager {
 
     /**
      * Return free credentials from pool
+     * @param {string} pool - name of pool
      * @return {Promise<Object>} - promise that resolves with set of credentials
      * @throws {Error}
-     * @example 
+     * @example
      * CredentialManager.getCredentials();
      * const currentCredentials = await CredentialManager.credentials;
      */
-    static getCredentials() {
+    static getCredentials(pool) {
+        let requestURI = SERVICE_URI;
+        if (pool) {
+            requestURI += "?pool=" + pool;
+        }
         this.credentials = request({
             method: "GET",
-            uri: SERVICE_URI,
+            uri: requestURI,
         })
-        .then(body => JSON.parse(body))
-        .catch(e => {
-            throw e
-        })
+            .then(body => JSON.parse(body))
+            .catch(e => {
+                throw e
+            })
     }
 
     /**
      * Free credentials
+     * @param {string} pool - name of pool
      * @throws {Error}
      * @example CredentialManager.freeCredentials();
      */
-    static freeCredentials() {
+    static freeCredentials(pool) {
+        let requestURI = SERVICE_URI;
+        if (pool) {
+            requestURI += "?pool=" + pool;
+        }
         return this.credentials.then(credentials => {
             if (credentials) {
                 return request({
                     method: "PUT",
-                    uri: SERVICE_URI,
+                    uri: requestURI,
                     body: {
                         username: credentials.username
                     },
@@ -70,17 +85,22 @@ class ServerCredentialManager {
 
     /**
      * Free credentials
+     * @param {string} pool - name of pool
      * @param property - property to update
      * @param value - value to update
      * @throws {Error}
      * @example CredentialManager.updateProperty("cookie", "myCookie");
      */
     static updateProperty(property, value) {
+        let requestURI = SERVICE_URI + "/update";
+        if (pool) {
+            requestURI += "?pool=" + pool;
+        }
         return this.credentials.then(credentials => {
             if (credentials) {
                 return request({
                     method: "PUT",
-                    uri: SERVICE_URI + "/update",
+                    uri: requestURI,
                     body: {
                         username: credentials.username,
                         property: property,
@@ -89,7 +109,8 @@ class ServerCredentialManager {
                     json: true
                 })
             }
-        }).catch(e => {
+        })
+        .catch(e => {
             throw e
         })
     }
@@ -97,6 +118,7 @@ class ServerCredentialManager {
     /**
      * Return specified credentials by username
      * @param username - username to get
+     * @param {string} pool - name of pool
      * @return {Promise<Object>} - promise that resolves with set of credentials
      * @throws {Error}
      * @example
@@ -104,9 +126,13 @@ class ServerCredentialManager {
      * const currentCredentials = await CredentialManager.credentials;
      */
     static getCredentialsByUsername(username) {
+        let requestURI = SERVICE_URI + "/" + username;
+        if (pool) {
+            requestURI += "?pool=" + pool;
+        }
         this.credentials = request({
             method: "GET",
-            uri: SERVICE_URI + "/" + username,
+            uri: requestURI,
         })
         .then(body => JSON.parse(body))
         .catch(e => {
